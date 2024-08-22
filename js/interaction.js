b23b0fe24a1fdf662fe9fbe0d0d111d038afa913
Mar972310 <maria.torres-m@mail.escuelaing.edu.co>
2024-08-21 21:35:49 -0500

    jsjsjsj se puede cerrar sesion

--- js/interaction.js
+++ js/interaction.js
@@ -17,3 +17,18 @@ function newSupplier() {
     window.location.href= "/html/j.html" 
 
 }
+
+function exit(){
+    const alert = document.getElementById("danger");
+    alert.style.display = "flex";
+}
+
+function acceptExit(){
+    window.location.href= "/html/login.html" 
+}
+
+function deniedExit(){
+    const alert = document.getElementById("danger");
+    alert.style.display = "none";
+}
+
